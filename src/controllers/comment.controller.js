f8929ab1a14e182d16723379a5770b7f34a6db11
Pedro Gutierrez Jr <pedrogutie0@gmail.com>
2024-05-28 14:41:58 -0500

    finished comment controllers and routes

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,5 +1,7 @@
 import Post from "../models/post.model.js";
 import Comment from "../models/comment.model.js";
+import User from "../models/user.model.js";
+import Notification from "../models/notification.model.js";
 
 export const getPostsComments = async (req, res) => {
   try {
@@ -43,7 +45,33 @@ export const getPostsComments = async (req, res) => {
     }
 
     res.status(500).json({ error: "Internal Server Error" });
-    console.log("Error in createComment controller:", errorMessage);
+    console.log("Error in getPostsComments controller:", errorMessage);
+  }
+};
+
+export const getLikedComments = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const user = await User.findById(userId);
+
+    const likedComments = await Comment.find({
+      _id: { $in: user.likedComments },
+    }).populate({
+      path: "user",
+      select: "-password",
+    });
+
+    res.status(200).json(likedComments);
+  } catch (error) {
+    let errorMessage = "Something went wrong";
+
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+
+    res.status(500).json({ error: "Internal Server Error" });
+    console.log("Error in getLikedComments controller:", errorMessage);
   }
 };
 
@@ -96,5 +124,91 @@ export const createComment = async (req, res) => {
 
     res.status(500).json({ error: "Internal Server Error" });
     console.log("Error in createComment controller:", errorMessage);
+  }
+};
+
+export const likeUnlikeComment = async (req, res) => {
+  try {
+    console.log("hey");
+    const userId = req.user._id;
+    const commentId = req.params.id;
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) return res.status(404).json({ error: "Comment not found" });
+
+    const userLikedComment = comment.likes.includes(userId);
+
+    if (userLikedComment) {
+      // Unlike comment
+      await Comment.updateOne({ _id: commentId }, { $pull: { likes: userId } });
+      await User.updateOne(
+        { _id: userId },
+        { $pull: { likedComments: commentId } }
+      );
+
+      const updatedLikes = comment.likes.filter(
+        (id) => id.toString() !== userId.toString()
+      );
+
+      res.status(200).json(updatedLikes);
+    } else {
+      // like comment
+      comment.likes.push(userId);
+      await User.updateOne(
+        { _id: userId },
+        { $push: { likedComments: commentId } }
+      );
+      await comment.save();
+
+      const notification = new Notification({
+        from: userId,
+        to: comment.user,
+        type: "comment",
+      });
+
+      await notification.save();
+
+      const updatedLikes = comment.likes;
+
+      res.status(200).json(updatedLikes);
+    }
+  } catch (error) {
+    let errorMessage = "Something went wrong";
+
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+
+    res.status(500).json({ error: "Internal Server Error" });
+    console.log("Error in likeUnlikeComment controller:", errorMessage);
+  }
+};
+
+export const deleteComment = async (req, res) => {
+  try {
+    const commentId = req.params.id.toString();
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) return res.status(404).json({ error: "Comment not found" });
+
+    if (comment.user.toString() !== req.user._id.toString())
+      return res
+        .status(401)
+        .json({ error: "You are not authorized to delete this comment" });
+
+    await Comment.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({ message: "Comment deleted successfully" });
+  } catch (error) {
+    let errorMessage = "Something went wrong";
+
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+
+    res.status(500).json({ error: "Internal Server Error" });
+    console.log("Error in deleteComment controller:", errorMessage);
   }
 };
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -3,11 +3,17 @@ import { protectRoute } from "../middleware/protectRoute.js";
 import {
   createComment,
   getPostsComments,
+  getLikedComments,
+  likeUnlikeComment,
+  deleteComment,
 } from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
 router.get("/:id", protectRoute, getPostsComments);
+router.get("/user/liked", protectRoute, getLikedComments);
 router.post("/:id", protectRoute, createComment);
+router.post("/like/:id", protectRoute, likeUnlikeComment);
+router.delete("/:id", protectRoute, deleteComment);
 
 export default router;
