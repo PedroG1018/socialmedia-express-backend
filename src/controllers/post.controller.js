589c22357d26b470bb70bebf09f3ba93de61084a
Pedro Gutierrez Jr <pedrogutie0@gmail.com>
2024-06-03 14:49:58 -0500

    added new route to /api/posts

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -26,6 +26,30 @@ export const getAllPosts = async (req, res) => {
   }
 };
 
+export const getPost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    const post = await Post.findById(postId).populate({
+      path: "user",
+      select: "-password",
+    });
+
+    if (!post) return res.status(404).json({ error: "Post not found" });
+
+    res.status(200).json(post);
+  } catch (error) {
+    let errorMessage = "Something went wrong";
+
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+
+    res.status(500).json({ error: "Internal server error" });
+    console.log("Error in getPost controller:", errorMessage);
+  }
+};
+
 export const getFollowingPosts = async (req, res) => {
   try {
     const userId = req.user._id;
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -4,6 +4,7 @@ import {
   createPost,
   deletePost,
   getAllPosts,
+  getPost,
   getFollowingPosts,
   getLikedPosts,
   getUsersPosts,
@@ -13,6 +14,7 @@ import {
 const router = express.Router();
 
 router.get("/", protectRoute, getAllPosts);
+router.get("/post/:id", protectRoute, getPost);
 router.get("/following", protectRoute, getFollowingPosts);
 router.get("/:username", protectRoute, getUsersPosts);
 router.get("/likes/:id", protectRoute, getLikedPosts);
